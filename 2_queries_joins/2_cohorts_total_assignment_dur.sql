ad15eed069d4af044549efb0ae3d326c2a1189ca
Jackson Fung <jacksonf21@hotmail.com>
2019-08-19 18:15:01 +0000

    add student cohorts join query

---
+++ 2_queries_joins/2_cohorts_total_assignment_dur.sql
@@ -0,0 +1,7 @@
+SELECT SUM(assignment_submissions.duration)
+  FROM cohorts
+  JOIN students 
+  ON (cohorts.id = students.cohort_id)
+  JOIN assignment_submissions
+  ON (students.id = assignment_submissions.student_id)
+  WHERE cohorts.name = 'FEB12';
