6649afd224b8590cb3852f75de06a42d1eaf62dc
Jackson Fung <jacksonf21@hotmail.com>
2019-08-21 17:54:56 +0000

    prevent sql injection

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -8,15 +8,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
-  SELECT DISTINCT teachers.name AS teacher ,cohorts.name AS cohort
-  FROM assistance_requests
+const queryString = `SELECT DISTINCT teachers.name AS teacher ,cohorts.name AS cohort
+FROM assistance_requests
   JOIN teachers ON teachers.id = teacher_id
   JOIN students ON students.id = student_id
   JOIN cohorts ON cohorts.id = cohort_id
-  WHERE cohorts.name LIKE '${process.argv[2]}%'
-  ORDER BY teachers.name 
-`)
+  WHERE cohorts.name LIKE $1
+  ORDER BY teachers.name`;
+const cohortsName = process.argv[2];
+const values = [`${cohortsName}%`];
+
+pool.query(queryString, values)
   .then(res => {
     res.rows.forEach(user => {
       console.log(`${user.cohort}: ${user.teacher}`);
